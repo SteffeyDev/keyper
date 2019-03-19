eb132e2871fd29e9219ed39bb9c5a7d1695595cb
Sundown <you@example.com>
2019-03-19 18:50:39 -0400

    added create user route

--- back-end/run.py
+++ back-end/run.py
@@ -1,11 +1,16 @@
 import re, os, datetime
-from flask import Flask
+from flask import Flask, request
 from mongoengine import *
+from mongoengine.context_managers import switch_collection
 
 app = Flask(__name__)
 
-# Database = keyper
+# db = keyper, mongodb://172.0.0.1:27017
 connect('keyper')
+
+# Encrypted site specific password blobs
+class siteInfo(EmbeddedDocument):
+	content = BinaryField()
 
 class User(Document):
 	username = StringField(max_length=64, required=True)
@@ -13,10 +18,14 @@ class User(Document):
 	password = StringField(min_length=12, required=True)
 	sites = ListField(EmbeddedDocumentField(siteInfo))
 
-# Encrypted site specific password blobs
-class siteInfo(EmbeddedDocument):
-	content = BinaryField()
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/insert', methods=['GET','POST'])
+def insert():
+	error = None
+	newUser = User(username=request.form['username'], email=request.form['email'], password=request.form['password'])
+	with switch_collection(User, 'users') as toAdd:
+		newUser.save(validate=True)
+	return 'New user added'
