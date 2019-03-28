3e2680ec47e8bb17efe5ead2a84779c781adb454
19csi <19csingh@gmail.com>
2019-03-28 02:43:41 -0400

    Added basic/empty activities

---
+++ Mobile App/app/src/main/java/com/example/keypermobile/TwoFactorAuthActivity.java
@@ -0,0 +1,13 @@
+package com.example.keypermobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwoFactorAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_factor_auth);
+    }
+}
