21f46ed65bb966cbfc87b55cfc106242e477d535
SteffeyDev <steffeydev@icloud.com>
2019-03-29 14:00:53 -0400

    Adding functionality to table

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -1,26 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipDefaultOptions } from '@angular/material';
 
-export interface PasswordEntry {
+function copyToClipboard(text: string) {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = text;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+}
+
+export class PasswordEntry {
   title?: string;
   url?: string;
-  usertitle?: string;
+  username?: string;
   email?: string;
   password?: string;
   tags?: [string];
   notes?: string;
+  passwordVisible: boolean;
+
+  constructor(title: string, url: string, username: string, email: string, password: string) {
+    this.title = title;
+    this.url = url;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+
+    this.passwordVisible = false;
+  }
+
+  showPassword() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  copyPassword() {
+    copyToClipboard(this.password);
+  }
+
+  copyUsername() {
+    copyToClipboard(this.username);
+  }
+
+  openURL() {
+
+  }
 }
 
 const PASSWORD_DATA: PasswordEntry[] = [
-  { title: 'Apple', url: 'apple.com', usertitle: 'test@icloud.com', password: 'iloveapple' },
-  { title: 'Google', url: 'google.com', usertitle: 'test@gmail.com', password: 'ilovegoogle' },
-  { title: 'Microsoft', url: 'microsoft.com', usertitle: 'test@outlook.com', password: 'ilovemicrosoft' },
+  new PasswordEntry('Apple', 'apple.com', 'test@icloud.com', null, 'iloveapple'),
+  new PasswordEntry('Google', 'google.com', 'test@gmail.com', null, 'ilovegoogle'),
+  new PasswordEntry('Microsoft', 'microsoft.com', 'test@outlook.com', null, 'iloveslowcomputers')
 ];
+
+const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
+  showDelay: 500,
+  hideDelay: 0,
+  touchendHideDelay: 0,
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [
+    {provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults}
+  ],
 })
 export class HomeComponent implements OnInit {
   displayedColumns: string[] = ['title', 'url', 'username', 'email', 'password'];
