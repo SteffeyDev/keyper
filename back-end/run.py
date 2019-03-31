23bdf749b88ea6ea4ea67321cdced934fa218dcd
Raheim Scantlebury <kiouseh@yahoo.com>
2019-03-30 20:15:31 -0400

    sessions removed, login with password completed
    Removed sessions in anticipation for flask-login, login using password works properly.

--- back-end/run.py
+++ back-end/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import re, os, datetime
+import json
 from flask import Flask, request, session
 from flask_bcrypt import Bcrypt
 import pyotp
@@ -18,7 +19,8 @@ if not SECRET_KEY:
 app.secret_key = SECRET_KEY
 
 # db = keyper, mongodb://172.0.0.1:27017
-connect('keyper')
+#test database below. Eventually replace with real database
+connect('keyper', host='mongodb://test:testUser1@ds161104.mlab.com:61104/practice')
 
 # Encrypted site specific password blobs
 class siteInfo(EmbeddedDocument):
@@ -40,45 +42,49 @@ def insert():
 	# Generate password hash, 12 rounds
 	pwHash = bcrypt.generate_password_hash(request.form['password'])
 	newUser = User(username=request.form['username'], email=request.form['email'], password=pwHash)
-	with switch_collection(User, 'users') as toAdd:
-		newUser.save(validate=True)
-	return 'New user added'
+	with switch_collection(User, 'users') as toGet: #Need to verify if this is the correct way to handle this
+		try:
+			if User.objects.get(username__exact = str(request.form['username'])):
+				return 'User already exists. Choose a different username, or try logging in.'
+
+		except:
+			with switch_collection(User, 'users') as toAdd:
+				newUser.save(validate=True)
+				return 'New user added'
 
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
-	if 'username' in session:
 		error = None
 		user = str(request.form['username'])
 		with switch_collection(User, 'users') as toDel:
 			User.objects(username=user).delete()
 			return 'User has been deleted'
 
-	return 'Not logged in!'
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	user = request.form['username']
-	User.objects(username = user)
-	#check_password_hash(pw_hash, password)
+	password = request.form['password']
+	with switch_collection(User, 'users') as toGet:
+		search = User.objects.get(username__exact = user)
+		if bcrypt.check_password_hash(search.password, password):
+			return 'Logged in as %s' %user
 
-	session['username'] = user #should be done after login
-	return 'Logged in as %s' %user
+		else:
+			return 'Incorrect password. Try again.'
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
 	return True
 
-@app.route('/getsession')
-def getsession():
-	if 'username' in session:
-		return session['username']
-
-	return 'Not logged in!'
-
-@app.route('/dropsession')
-def dropsession():
-	session.pop('username', None)
-	return 'Dropped from session!'
+@app.route('/retrieve',  methods=['GET', 'POST'])
+def retrieve():
+	user = str(request.form['username'])
+	try:
+		with switch_collection(User, 'users') as toGet:
+			search = User.objects.get(username__exact = user)
+			return search.to_json()
+	except:
+		return 'User not found.'
 
 if __name__ == '__main__':
 	app.run()
