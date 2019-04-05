f457bd929d390656c28a1e4cbf9f190c4e2477ed
Raheim Scantlebury <kiouseh@yahoo.com>
2019-04-04 21:13:19 -0400

    Added flask-login + misc fixes
    Added flask-login and fixed some logic on routes. Working on login_required for certain routes.

--- back-end/run.py
+++ back-end/run.py
@@ -4,19 +4,23 @@ import re, os, datetime
 import json
 from flask import Flask, request, session
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, logout_user, login_required
 import pyotp
 from flask_jwt import JWT, jwt_required, current_identity
 from mongoengine import *
 from mongoengine.context_managers import switch_collection
 
 app = Flask(__name__)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
 bcrypt = Bcrypt(app)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 if not SECRET_KEY:
 	raise ValueError("No secret key set in OS for API")
 
-app.secret_key = SECRET_KEY
+app.config['SECRET_KEY'] = SECRET_KEY
 
 # db = keyper, mongodb://172.0.0.1:27017
 #test database below. Eventually replace with real database
@@ -32,6 +36,27 @@ class User(Document):
 	password = StringField(required=True)
 	sites = ListField(EmbeddedDocumentField(siteInfo))
 
+	def is_authenticated(self):
+		return True
+
+	def is_active(self):
+		return True
+
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return self.username
+
+@lm.user_loader
+def load_user(username):
+	with switch_collection(User, 'users') as toGet:
+		u = User.objects.get(username__exact = username)
+		if not u:
+			return None
+
+		return u.to_json()
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -42,12 +67,12 @@ def insert():
 	# Generate password hash, 12 rounds
 	pwHash = bcrypt.generate_password_hash(request.form['password'])
 	newUser = User(username=request.form['username'], email=request.form['email'], password=pwHash)
-	with switch_collection(User, 'users') as toGet: #Need to verify if this is the correct way to handle this
+	with switch_collection(User, 'users') as toGet:
 		try:
 			if User.objects.get(username__exact = str(request.form['username'])):
 				return 'User already exists. Choose a different username, or try logging in.'
 
-		except:
+		except DoesNotExist:
 			with switch_collection(User, 'users') as toAdd:
 				newUser.save(validate=True)
 				return 'New user added'
@@ -65,16 +90,17 @@ def login():
 	user = request.form['username']
 	password = request.form['password']
 	with switch_collection(User, 'users') as toGet:
-		search = User.objects.get(username__exact = user)
-		if bcrypt.check_password_hash(search.password, password):
-			return 'Logged in as %s' %user
+		try:
+			search = User.objects.get(username__exact = user)
+			if bcrypt.check_password_hash(search.password, password):
+				login_user(search)
+				return 'Logged in as %s' %user
 
-		else:
-			return 'Incorrect password. Try again.'
+			else:
+				return 'Invalid password. Try again.'
 
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-	return True
+		except DoesNotExist:
+			return 'User does not exist. Try again or register.'
 
 @app.route('/retrieve',  methods=['GET', 'POST'])
 def retrieve():
@@ -85,6 +111,11 @@ def retrieve():
 			return search.to_json()
 	except:
 		return 'User not found.'
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+	logout_user()
+	return "Logged out successfully."
 
 if __name__ == '__main__':
 	app.run()
