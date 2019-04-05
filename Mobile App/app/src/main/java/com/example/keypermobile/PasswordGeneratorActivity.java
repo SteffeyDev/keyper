fd919b59d65572bd9f6b416177a633c270e810b7
19csi <19csingh@gmail.com>
2019-04-05 11:15:40 -0400

    Setup PasswordGenerator layout

--- Mobile App/app/src/main/java/com/example/keypermobile/PasswordGeneratorActivity.java
+++ Mobile App/app/src/main/java/com/example/keypermobile/PasswordGeneratorActivity.java
@@ -1,29 +1,75 @@
 package com.example.keypermobile;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Switch;
+import android.widget.SeekBar;
 
 public class PasswordGeneratorActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TextView textViewGenerated;
+    TextView textViewLength;
+    Switch switchNumbers;
+    Switch switchLetters;
+    Switch switchSpecialCharacters;
+    SeekBar seekBarLength;
+
+
+    // Event Handlers
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_generator);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        textViewGenerated = findViewById(R.id.textViewGenerated);
+        textViewLength = findViewById(R.id.textViewLength);
+
+        switchNumbers = findViewById(R.id.switchNumbers);
+        switchNumbers.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        switchLetters = findViewById(R.id.switchLetters);
+        switchLetters.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        switchSpecialCharacters = findViewById(R.id.switchSpecialCharacters);
+        switchSpecialCharacters.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        seekBarLength = findViewById(R.id.seekBarLength);
+        seekBarLength.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
-
 }
