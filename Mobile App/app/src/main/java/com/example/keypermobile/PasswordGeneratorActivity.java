8ca7e81da5b8fe20ada46272d3de4da6469b8ac9
19csi <19csingh@gmail.com>
2019-04-11 16:58:37 -0400

    Updated layout and activity for Password Generator

--- Mobile App/app/src/main/java/com/example/keypermobile/PasswordGeneratorActivity.java
+++ Mobile App/app/src/main/java/com/example/keypermobile/PasswordGeneratorActivity.java
@@ -1,34 +1,78 @@
 package com.example.keypermobile;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Switch;
 import android.widget.SeekBar;
 
 public class PasswordGeneratorActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+
+    NavigationView navigationView;
+
+    Menu navigationViewMenu;
+
     Toolbar toolbar;
+
     TextView textViewGenerated;
+    TextView textViewGeneratedPassword;
     TextView textViewLength;
+
     Switch switchNumbers;
     Switch switchLetters;
     Switch switchSpecialCharacters;
+
     SeekBar seekBarLength;
 
+    ImageButton imageButtonCopyGenerated;
+    ImageButton imageButtonOpenDrawer;
 
-    // Event Handlers
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_password_generator);
+        setContentView(R.layout.navigation_drawer_password_generator);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+
         textViewGenerated = findViewById(R.id.textViewGenerated);
         textViewLength = findViewById(R.id.textViewLength);
+        textViewGeneratedPassword = findViewById(R.id.textViewGeneratedPassword);
+
+        navigationView = findViewById(R.id.nav_view_password_generator);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                // highlight selected item
+                menuItem.setChecked(true);
+
+                //close drawer when item is selected
+                drawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
+
+        navigationViewMenu = navigationView.getMenu();
 
         switchNumbers = findViewById(R.id.switchNumbers);
         switchNumbers.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -71,5 +115,34 @@ public class PasswordGeneratorActivity extends AppCompatActivity {
 
             }
         });
+
+        imageButtonCopyGenerated = findViewById(R.id.imageButtonCopyGenerated);
+        imageButtonCopyGenerated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText("Generated Password", textViewGeneratedPassword.getText());
+                clipboardManager.setPrimaryClip(clipData);
+            }
+        });
+
+        imageButtonOpenDrawer = findViewById(R.id.imageButtonOpenDrawer);
+        imageButtonOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+           }
+        });
+    }
+
+    // Menu item is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
