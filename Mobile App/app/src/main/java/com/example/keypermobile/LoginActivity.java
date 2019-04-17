df910286a6cd0c1d1592d8c97982a99b0b5c1a30
19csi <19csingh@gmail.com>
2019-04-17 12:47:20 -0400

    temp change to test transition to Edit Password

--- Mobile App/app/src/main/java/com/example/keypermobile/LoginActivity.java
+++ Mobile App/app/src/main/java/com/example/keypermobile/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.example.keypermobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.button7);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent genIntent = new Intent(getApplicationContext(), EditPasswordActivity.class);
+                startActivity(genIntent);
+            }
+        });
     }
 }
