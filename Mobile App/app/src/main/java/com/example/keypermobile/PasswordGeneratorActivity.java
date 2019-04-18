e633a8c2917ddf6bea83b234b48161d3ced5c9a4
19csi <19csingh@gmail.com>
2019-04-18 16:54:49 -0400

    added navigation drawer functionality, fixed characterSet bug

--- Mobile App/app/src/main/java/com/example/keypermobile/PasswordGeneratorActivity.java
+++ Mobile App/app/src/main/java/com/example/keypermobile/PasswordGeneratorActivity.java
@@ -3,6 +3,7 @@ package com.example.keypermobile;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -20,12 +21,9 @@ import android.widget.TextView;
 import android.widget.Switch;
 import android.widget.SeekBar;
 import android.widget.Toast;
-
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Random;
 
-public class PasswordGeneratorActivity extends AppCompatActivity implements IPasswordGenerator{
+public class PasswordGeneratorActivity extends AppCompatActivity implements IPasswordGenerator {
 
     DrawerLayout drawerLayout;
 
@@ -48,8 +46,6 @@ public class PasswordGeneratorActivity extends AppCompatActivity implements IPas
     ImageButton imageButtonCopyGenerated;
     ImageButton imageButtonOpenDrawer;
 
-    int passwordLength = DEFAULT_PASSWORD_LENGTH;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,22 +54,34 @@ public class PasswordGeneratorActivity extends AppCompatActivity implements IPas
         setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
 
         textViewGenerated = findViewById(R.id.textViewGenerated);
         textViewLength = findViewById(R.id.textViewLength);
         textViewGeneratedPassword = findViewById(R.id.textViewGeneratedPassword);
 
-        navigationView = findViewById(R.id.nav_view_password_generator);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 // highlight selected item
                 menuItem.setChecked(true);
 
+                if (menuItem.getItemId() == R.id.item_home)
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                else if (menuItem.getItemId() == R.id.item_tags)
+                    // Might change, unsure where to send on tags
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                else if (menuItem.getItemId() == R.id.item_log_out)
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                // User is in password generator, so no need to check
+
                 //close drawer when item is selected
                 drawerLayout.closeDrawers();
+
+                navigationView.setCheckedItem(R.id.menu_none);
 
                 return true;
             }
@@ -82,47 +90,57 @@ public class PasswordGeneratorActivity extends AppCompatActivity implements IPas
         navigationViewMenu = navigationView.getMenu();
 
         switchNumbers = findViewById(R.id.switchNumbers);
+        switchNumbers.setChecked(true);
         switchNumbers.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                textViewGeneratedPassword.setText(generatePassword(passwordLength, getCharacterSet()));
+                textViewGeneratedPassword.setText(generatePassword(seekBarLength.getProgress(), getCharacterSet()));
             }
         });
 
         switchLetters = findViewById(R.id.switchLetters);
+        switchLetters.setChecked(true);
         switchLetters.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                textViewGeneratedPassword.setText(generatePassword(passwordLength, getCharacterSet()));
+                textViewGeneratedPassword.setText(generatePassword(seekBarLength.getProgress(), getCharacterSet()));
             }
         });
 
         switchSpecialCharacters = findViewById(R.id.switchSpecialCharacters);
+        switchSpecialCharacters.setChecked(true);
         switchSpecialCharacters.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                textViewGeneratedPassword.setText(generatePassword(passwordLength, getCharacterSet()));
+                textViewGeneratedPassword.setText(generatePassword(seekBarLength.getProgress(), getCharacterSet()));
             }
         });
+
+        final TextView textViewProgress = findViewById(R.id.progress);
+        textViewProgress.setVisibility(View.GONE);
 
         seekBarLength = findViewById(R.id.seekBarLength);
         seekBarLength.setMax((SEEK_BAR_MAX - SEEK_BAR_MIN)/1);
         seekBarLength.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // Change the value for the generated password length, update generated password
-                passwordLength = SEEK_BAR_MAX + (progress * SEEK_BAR_STEP);
-                textViewGeneratedPassword.setText(generatePassword(passwordLength, getCharacterSet()));
+                // Update generated password
+                textViewGeneratedPassword.setText(generatePassword(seekBarLength.getProgress(), getCharacterSet()));
+                int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                textViewProgress.setText("" + progress);
+                textViewProgress.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 // only gen new password when seekbar moves
+                textViewProgress.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 // only gen new password when seekbar moves
+                textViewProgress.setVisibility(View.GONE);
             }
         });
 
@@ -130,11 +148,7 @@ public class PasswordGeneratorActivity extends AppCompatActivity implements IPas
         imageButtonCopyGenerated.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clipData = ClipData.newPlainText("Generated Password", textViewGeneratedPassword.getText());
-                clipboardManager.setPrimaryClip(clipData);
-                // Using toast to notify user the text has been copied
-                Toast.makeText(getApplicationContext(), "Copied", Toast.LENGTH_SHORT);
+                CopyPassword();
             }
         });
 
@@ -145,33 +159,24 @@ public class PasswordGeneratorActivity extends AppCompatActivity implements IPas
                 drawerLayout.openDrawer(GravityCompat.START);
            }
         });
-    }
 
-    // Menu item is selected
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+        // Generate password
+        textViewGeneratedPassword.setText(generatePassword(seekBarLength.getProgress(), getCharacterSet()));
     }
 
     // Generate password using the user inputs in this activity
     @Override
     public String generatePassword(int length, String characterSet)
     {
-        // Finish method by checking switch cases and choosing accordingly
         SecureRandom random = new SecureRandom();
-        String generatedPassword = "";
+        StringBuilder generatedPassword = new StringBuilder(length);
 
         for (int i = 0; i < length; i++)
         {
-            int randomInt = random.nextInt(length);
-            generatedPassword.concat(Character.toString(characterSet.charAt(randomInt)));
+            int randomIndex = random.nextInt(characterSet.length());
+            generatedPassword.append(characterSet.charAt(randomIndex));
         }
-        return generatedPassword;
+        return generatedPassword.toString();
     }
 
     // return the characterSet corresponding to the checked switches
@@ -189,8 +194,25 @@ public class PasswordGeneratorActivity extends AppCompatActivity implements IPas
             return LETTERS;
         else if (switchNumbers.isChecked())
             return NUMBERS;
-        else
+        else if (switchSpecialCharacters.isChecked())
             return SPECIAL_CHARACTERS;
+        // No switches are checked
+        else
+        {
+            switchLetters.setChecked(true);
+            // notify user at least one character set must be enabled
+            Toast.makeText(getApplicationContext(), "One character set must be enabled", Toast.LENGTH_SHORT).show();
+            return LETTERS;
+        }
     }
 
+    // Copy password to the clipboard and notify the user
+    public void CopyPassword()
+    {
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clipData = ClipData.newPlainText("Generated Password", textViewGeneratedPassword.getText());
+        clipboardManager.setPrimaryClip(clipData);
+        // Using toast to notify user the text has been copied
+        Toast.makeText(getApplicationContext(), "Password Copied", Toast.LENGTH_SHORT).show();
+    }
 }
