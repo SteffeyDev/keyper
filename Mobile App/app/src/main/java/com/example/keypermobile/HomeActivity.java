ab8c17e781f69bdb3183b03fb33c9c7c0d94e4aa
19csi <19csingh@gmail.com>
2019-04-20 22:02:36 -0400

    added click functionality to cards and added add button

--- Mobile App/app/src/main/java/com/example/keypermobile/HomeActivity.java
+++ Mobile App/app/src/main/java/com/example/keypermobile/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.keypermobile;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,10 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.keypermobile.models.Password;
 import com.example.keypermobile.utils.PasswordAdapter;
@@ -18,7 +24,7 @@ import com.example.keypermobile.utils.PasswordAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements PasswordAdapter.OnPasswordClickListener{
     Toolbar toolbar;
     RecyclerView recyclerView;
     PasswordAdapter adapter;
@@ -27,18 +33,32 @@ public class HomeActivity extends AppCompatActivity {
 
     NavigationView navigationView;
 
+    ImageButton imageButtonOpenDrawer;
+
     Menu navigationViewMenu;
 
     List<Password> passwordList;
 
+    FloatingActionButton floatingActionButtonAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.navigation_drawer_home);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Keyper");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        floatingActionButtonAdd = findViewById(R.id.floatingActionButtonAdd);
+        floatingActionButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addPasswordIntent = new Intent(getApplicationContext(), EditPasswordActivity.class);
+                addPasswordIntent.putExtra("Activity Title", getResources().getString(R.string.title_activity_add_password));
+                startActivity(addPasswordIntent);
+            }
+        });
 
         drawerLayout = findViewById(R.id.drawer_layout);
 
@@ -69,6 +89,14 @@ public class HomeActivity extends AppCompatActivity {
 
         navigationViewMenu = navigationView.getMenu();
 
+        imageButtonOpenDrawer = findViewById(R.id.imageButtonOpenDrawer);
+        imageButtonOpenDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
         passwordList = new ArrayList<>();
 
         recyclerView = findViewById(R.id.recyclerView);
@@ -96,9 +124,19 @@ public class HomeActivity extends AppCompatActivity {
         passwordList.add(
                 new Password(7,"Desmos","www.Desmos.com", R.drawable.logo_google)
         );
-        adapter = new PasswordAdapter(this, passwordList);
+        adapter = new PasswordAdapter(this, passwordList, this);
         recyclerView.setAdapter(adapter);
     }
+
+        @Override
+        public void onPasswordClick ( int position){
+            passwordList.get(position);
+            Intent editPasswordIntent = new Intent(getApplicationContext(), EditPasswordActivity.class);
+            editPasswordIntent.putExtra("Title", passwordList.get(position).getTitle());
+            editPasswordIntent.putExtra("Website", passwordList.get(position).getWebsite());
+            editPasswordIntent.putExtra("Activity Title", getResources().getString(R.string.title_activity_edit_password));
+            startActivity(editPasswordIntent);
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -121,4 +159,5 @@ public class HomeActivity extends AppCompatActivity {
 //        Toast.makeText(this, msg + " Checked", Toast.LENGTH_LONG).show();
         return super.onOptionsItemSelected(item);
     }
+
 }
